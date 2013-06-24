d3392b4f94b384de0aba64da77730a9e5c20c130
Seymour Knowles-Barley <seymourkb@seas.harvard.edu>
2013-06-24 10:04:37 -0400

    bug fix

--- Mojo.2.0/Mojo/Mojo.Wpf/View/D3D11RenderingPaneHost.cs
+++ Mojo.2.0/Mojo/Mojo.Wpf/View/D3D11RenderingPaneHost.cs
@@ -64,14 +64,6 @@ namespace Mojo.Wpf.View
             }
         }
 
-        protected void OnManipulationDelta( System.Windows.Input.ManipulationDeltaEventArgs e ) 
-        {
-            if ( ViewerContext != null )
-            {
-                ViewerContext.OnManipulationDelta( e, Width, Height );
-            }
-        }
-
         public System.Drawing.SizeF GetAutoScaleFactor()
         {
             return AutoScaleFactor;
